43763756f31e44b07fe242536722960bf2a2b1df
greenrenge <greenrenge@gmail.com>
2019-10-03 20:44:48 +0700

    fix url

--- src/config.js
+++ src/config.js
@@ -1,5 +1,4 @@
-const APP_URL = process.env.URL.endsWith('/') ? process.env.URL.split('/').filter((_, i, arr) => i !== arr.length - 1).join('/') : process.env.URL
 
 module.exports = {
-    APP_URL
+    APP_URL: 'https://greenurl.herokuapp.com'
 }
