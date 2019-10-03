95750166131bf3740d6ec2c8515f3ea0d6739442
greenrenge <greenrenge@gmail.com>
2019-10-03 21:09:17 +0700

    api did not return string

--- src/helpers/url-generator.js
+++ src/helpers/url-generator.js
@@ -1,9 +1,8 @@
 import axios from 'axios'
 export async function createShortUrl(link) {
     if (!link) throw Error('Please input the correct url')
-    const shortenUrl = await axios.post(`/api`, {
+    const { shorten_url: shortenUrl } = await axios.post(`/api`, {
         link
     })
-    console.log('GREEN==> ', JSON.stringify(shortenUrl))
     return shortenUrl
 }
