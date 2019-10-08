5ff5a9cc07be283ab98d5c37ff27af924fdc5032
greenrenge <greenrenge@gmail.com>
2019-10-09 00:14:01 +0700

    auto put http

--- server/controllers/url.controller.js
+++ server/controllers/url.controller.js
@@ -2,16 +2,47 @@ const UrlModel = require('../models/url.model')
 const IdModel = require('../models/identifier.model')
 const base62 = require('base62')
 const config = require('../config/env')
+const rp = require('request-promise')
 const PREFIX = config.prefix.endsWith('/') ? config.prefix.split('/').filter((_, i, arr) => i !== arr.length - 1).join('/') : config.prefix
+
+const timeOut = ms => new Promise((resolve, reject) => {
+    setTimeout(reject, ms)
+})
 const isIdValid = id => id
 
+const tryRequest = async uri => {
+    const response = await rp({
+        uri,
+        method: 'GET',
+        resolveWithFullResponse: true
+    })
+
+    return /^2/.test('' + response.statusCode)
+}
+
+const taskTimeout = sec => task => async () => {
+    await Promise.race([
+        timeOut(sec),
+        task()
+    ])
+}
+
+const isLinkValid = async function (link = '') {
+    let tryList = []
+    if (link.startsWith('http')) {
+        tryList.push(link)
+    } else {
+        tryList.push(`https://${link}`)
+        tryList.push(`https://${link}`)
+    }
+}
 async function getOne(req, res, next) {
     try {
         const id = req.params.id
         if (isIdValid(id)) {
             const url = await UrlModel.findOne({ _id: `${id}` })
             if (url) {
-                return res.redirect(url.full_link)
+                return res.redirect(url.full_link.startsWith('http') ? url.full_link : `http://${url.full_link}`)
             }
         }
         return res.status(404).send('The link you are looking for is not found in the system')
